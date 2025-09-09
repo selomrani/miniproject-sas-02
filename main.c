9ba62b3672693d280c7930adb19b31702823a85e
Soufyane <elomranisoufyan@gmail.com>
2025-09-09 10:42:51 +0100

    Added all necessery functionalities / final version

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #define MAX_GRADES 5
 #define MAX_STUDENTS 30
+
 float calculate_average(float grades[], int num_grades) {
     float sum = 0.0;
     for (int i = 0; i < num_grades; i++) {
@@ -24,6 +25,7 @@ int main(void) {
     student students[MAX_STUDENTS];
     int num_grades = 0;
     int num_students = 0;
+    float sum;
 
     do {
         printf("   Welcome to grades manager v2.0\n");
@@ -51,14 +53,16 @@ int main(void) {
                 }
                 printf("how many students do you want to enter? : ");
                 scanf("%d", &num_students);
+                while(getchar() != '\n'); 
                 printf("Please fill in the following information:\n");
                 for ( int i = 0 ; i < num_students ; i++){
-                    printf("Enter name of student [%d] : \n",i+1);
-                    scanf("%s", students[i].name);
-                    printf("Enter age of student [%d] : \n",i+1);
-                    scanf("%d", &students[i].age);
-                    printf("Enter ID of student [%d] : \n",i+1);
-                    scanf("%s", students[i].id);
+                printf("Enter name of student [%d] : \n",i+1);
+                fgets(students[i].name, sizeof(students[i].name), stdin); 
+                printf("Enter age of student [%d] : \n",i+1);
+                scanf("%d", &students[i].age);
+                printf("Enter ID of student [%d] : \n",i+1);
+                scanf("%s", students[i].id);
+                while(getchar() != '\n');
                 }
                 break;
                 
@@ -71,15 +75,20 @@ int main(void) {
                 int student_index = -1;
                 printf("Select a student to add grades for (1 to %d): ", num_students);
                 scanf("%d",&student_index);
-                printf("How many grades do you want to enter? : ");
+                getchar();
+                printf("How many grades do you want to enter? : \n");
                 scanf("%d", &num_grades);
                 if (num_grades > MAX_GRADES) {
                     printf("You can only enter up to %d grades.\n", MAX_GRADES);
+                    break;
                 }
                 for ( int i = 0 ; i < num_grades ; i++){
                     printf("Enter grade [%d] : \n",i+1);
-                    scanf("%d", &students[student_index].grades[i]);
-
+                    scanf("%f", &students[student_index-1].grades[i]);
+                    if ( students[student_index-1].grades[i] < 0 || students[student_index-1].grades[i] > 20 ) {
+                        printf("You cant add a grade smaller than 0 or greater than 20 !\n");
+                        i--;
+                    }
                 }   
                 break;                          
                 
@@ -103,11 +112,9 @@ int main(void) {
                 int report_index = -1;
                 printf("Select a student to view report (1 to %d): ", num_students);
                 scanf("%d",&report_index);
-                printf("Report for %s:\n", students[report_index].name);
-                printf("Age: %d, ID: %s\n", students[report_index].age, students[report_index].id);
-                for (int i = 0; i < num_grades; i++) {
-                printf("Grade [%d]: %.2f\n", i+1, students[report_index].grades[i]);
-                }
+                printf("Report for %s:\n", students[report_index-1].name);
+                printf("Age: %d, ID: %s\n", students[report_index-1].age, students[report_index-1].id);
+                printf("average: %.2f\n", calculate_average(students[report_index-1].grades, num_grades));
                 break;
                 
             case 5: 
@@ -119,21 +126,28 @@ int main(void) {
                 int avg_index = -1;
                 printf("Select a student to calculate average (1 to %d): ", num_students);
                 scanf("%d",&avg_index);
-                students[avg_index].average = calculate_average(students[avg_index].grades, num_grades);
-                printf("Average grade for %s: %.2f\n", students[avg_index].name, students[avg_index].average);
+                students[avg_index-1].average = calculate_average(students[avg_index-1].grades, num_grades);
+                printf("Average grade for %s: %.2f\n", students[avg_index-1].name, students[avg_index-1].average);
                 break;
 
             case 6:
                 printf("Calculating class average of the whole class.\n");
+                for ( int i=0 ; i < num_students ; i++){
+                sum += students[i].average;
+                }
+                float avg_class;
+                avg_class = sum / num_students;
+                printf("The average grade of the whole class : %2.f\n",avg_class);
+                break;
                 
             case 7:
-            exit(0);
                 printf("Exiting the program.....\n");
+                exit(0);
                 break;
                 
             default:
-                printf("Invalid choice, please try again.\n");
-                break;  
+            printf("Invalid choice, please try again.\n");
+            break;  
         }
         printf("\n");
         
